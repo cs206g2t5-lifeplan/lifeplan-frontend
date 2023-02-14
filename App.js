4d24fe812b7121c7788d17ac1b893230561a8326
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2023-02-14 19:28:14 +0800

    tailwind web support
    transpile tailwind to css

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './dist/output.css'; // Add this for web support ONLY
 
 export default function App() {
 	return (
